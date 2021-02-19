ddd2bc56e183c22a47c22a3d20b0efaf85a8b718
Rio-olina <77527798+Rio-olina@users.noreply.github.com>
2021-02-20 01:10:52 +0300

    fix: change the code

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,12 +1,11 @@
-//const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
+  
   const cat = '^^';
-	let arr1 = matrix.concat(...matrix)
   let sum = 0;
-  for(i=0; i<arr1.length; i++){
-    if (arr1[i]=== cat){sum++}
-  }return sum;
- // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+    matrix.forEach((subArray) => {
+    subArray.forEach((item) => {
+  
+  if (item=== cat) {sum++} })
+  })
+   return sum
+}
