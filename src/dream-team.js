66f1c624209994a93ac1948cf23b0e9d032f2f43
Rio-olina <77527798+Rio-olina@users.noreply.github.com>
2021-02-22 12:26:10 +0300

    fix: change the code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,16 +1,14 @@
 module.exports = function createDreamTeam(/* members */) {
-	  let teamName = [];
-  if (members.length === []) {return false};
-  //if (Array.isArray(members)){
-    members.forEach(item=> {
-     if (typeof(item) !== 'string') {return false} else {
-   teamName.push(item.trim()[0])};
-     });
-	if (teamName.length = 0) {return false}
-	else
-	{arr = teamName.sort().join('');
-		 arr.toUpperCase();
-	//if (arr.length = 0) {return false}
 	 
-  
-	return arr} };
+  let message = false;
+  let teamName = [];
+  if (Array.isArray(members)){
+    members.forEach(item=> {
+    
+if (typeof(item) !== 'string') throw message; else {
+ 
+  teamName.push(item.trim()[0].toUpperCase())};
+     });
+    arr = teamName.sort().join('');
+  //  });
+ return arr };
